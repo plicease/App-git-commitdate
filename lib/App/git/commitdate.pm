96d9bb39a8b04b9be5093e4b68766993ff48c8a3
Graham Ollis <perl@wdlabs.com>
2015-09-17 06:42:07 -0400

    initial implementation

--- lib/App/git/commitdate.pm
+++ lib/App/git/commitdate.pm
@@ -1,9 +1,37 @@
-package App::git::commitdate;
-
 use strict;
 use warnings;
+use 5.020;
+use experimental qw( signatures postderef );
 
-# ABSTRACT: Set the date on a commit
-# VERSION
+package App::git::commitdate {
+
+  # ABSTRACT: Set the date on a commit
+  # VERSION
+
+  use Time::Local qw( timelocal );
+  use namespace::autoclean;
+
+  sub main($class, $date, @commit_args) {
+    if(defined $date) {
+      if($date =~ /^(?<year>[0-9]{4})-(?<month>[0-9]{1,2})-(?<day>[0-9]{1,2})$/) {
+        my $sec  = int rand 60;
+        my $min  = int rand 60;
+        my $hour = 7 + int rand 10;
+        #say "$hour:$min:$sec";
+        $date = timelocal $sec, $min, $hour, $+{day}, $+{month}, $+{year};
+      } else {
+        say STDERR "does not look like a date: $date";
+        return 2;
+      }
+    } else {
+      say STDERR "usage: git commit-date yyyy-mm-dd [commit flags]";
+      return 2;
+    }
+
+    say "% git commit --date=$date @commit_args";
+    exec 'git', 'commit', "--date=$date", @commit_args;
+  }
+
+}
 
 1;
